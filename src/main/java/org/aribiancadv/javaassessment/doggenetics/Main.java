c8321b6f5fde2ef919f87c0f2050c1d276a285f8
Ari Bianca DV <103449903+aribiancadv@users.noreply.github.com>
2022-08-16 15:55:35 +0100

    Completed Dog Genetics

--- src/main/java/org/aribiancadv/javaassessment/doggenetics/Main.java
+++ src/main/java/org/aribiancadv/javaassessment/doggenetics/Main.java
@@ -1,9 +1,45 @@
 package org.aribiancadv.javaassessment.doggenetics;
 
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Random randomizer = new Random();
+        Scanner userInput = new Scanner(System.in);
+        int[] numList = new int[5];
+
+
+        String dogName;
+        int tempSum = 0;
+
+        System.out.println("What is your dog's name?");
+        dogName = userInput.nextLine();
+
+        System.out.println("Well then, I have this highly reliable report on " + dogName + "'s prestigious background right here.");
+        System.out.println(dogName + " is:");
+        System.out.println();
+        numList[0] = randomizer.nextInt(20 - 1) + 1;
+        tempSum += numList[0];
+
+        for(int i = 1; i < numList.length - 1; i++){
+            numList[i] = randomizer.nextInt(20 - 1) + 1;
+            tempSum += numList[i];
+        }
+
+        numList[4] = 100 - tempSum;
+
+        System.out.println(numList[0] + "% St. Bernard");
+        System.out.println(numList[1] + "% Chihuahua");
+        System.out.println(numList[2] + "% Dramatic RedNosed Asian Pug");
+        System.out.println(numList[3] + "% Common Cur");
+        System.out.println(numList[4] + "% King Doberman");
+        System.out.println("Wow, that's QUITE the dog!");
+
+
     }
 
 }
