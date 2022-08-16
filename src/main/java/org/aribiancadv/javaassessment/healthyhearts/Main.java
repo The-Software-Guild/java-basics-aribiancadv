270bae99e8816d38aac8d9e7d792bc46e6155add
Ari Bianca DV <103449903+aribiancadv@users.noreply.github.com>
2022-08-16 16:21:06 +0100

    Completed Dog Genetics

--- src/main/java/org/aribiancadv/javaassessment/healthyhearts/Main.java
+++ src/main/java/org/aribiancadv/javaassessment/healthyhearts/Main.java
@@ -1,9 +1,26 @@
 package org.aribiancadv.javaassessment.healthyhearts;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner userInput = new Scanner(System.in);
+        int userAge;
+        int minHeartRateZone;
+        int maxHeartRateZone;
+        int maxHeartRate;
+
+        System.out.println("What is your age?");
+        userAge = Integer.parseInt(userInput.nextLine());
+        maxHeartRate = 220 - userAge;
+
+        System.out.println("Your maximum heart rate should be " + maxHeartRate + " beats per minute");
+        minHeartRateZone = (int)Math.round(0.5 * maxHeartRate);
+        maxHeartRateZone = (int)Math.round(0.85 * maxHeartRate);
+
+        System.out.println("Your target HR Zone is "+ minHeartRateZone + " - " + maxHeartRateZone + " beats per minute");
     }
 
 }
